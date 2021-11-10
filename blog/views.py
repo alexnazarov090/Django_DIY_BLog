327aff92387f1c48749dea64095df8c38bb4a8f1
Alex Nazarov <sladkii_bubaleg@yahoo.com>
2021-11-10 23:28:22 +0300

    Added base_generic, index html, views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,24 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import BlogAuthor, BlogPost, Comment
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<p>This DIY Blog!</p>")
+    # Number of blog posts
+    num_of_blog_posts = BlogPost.objects.count()
+
+    # Number of blog authors
+    num_of_bloggers = BlogAuthor.objects.count()
+
+    # Number of comments
+    num_of_comments = Comment.objects.count()
+
+    context = {
+                'num_of_blog_posts': num_of_blog_posts,
+                'num_of_bloggers': num_of_bloggers,
+                'num_of_comments': num_of_comments
+    }
+
+
+    return render(request, 'blog/index.html', context=context)
