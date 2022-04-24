58a7a12cb002dec8c0373fad5cf67c19b89b49e0
Alex Nazarov <sladkii_bubaleg@yahoo.com>
2022-04-24 18:05:00 +0300

    Added files and changes required for deployment to heroku

--- diyblog/settings/__init__.py
+++ diyblog/settings/__init__.py
@@ -1,14 +1,15 @@
 import os
 import environ
-
+import socket
 from diyblog.settings.base import *
 
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env.dev'))
+# env = environ.Env()
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env.dev'))
 
-if env("PROJECT") == "prod":
+# if env("PROJECT") == "prod":
+if socket.gethostname() != "DESKTOP-98M2CO0":
    from .production import *
 else:
    from .local import *
--- diyblog/settings/production.py
+++ diyblog/settings/production.py
@@ -30,20 +30,29 @@ INSTALLED_APPS += [
     'users.apps.UsersConfig',
 ]
 
+MIDDLEWARE += [
+    'WhiteNoiseMiddleware',
+    ]
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': env("SQL_ENGINE", default='django.db.backends.postgresql'),
-        'NAME': env("SQL_DATABASE", default='diy_blog_prod'),
-        "USER": env("SQL_USER", default="user"),
-        "PASSWORD": env("SQL_PASSWORD", default="password"),
-        "HOST": env("SQL_HOST", default="localhost"),
-        "PORT": env("SQL_PORT", default="5432"),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': env("SQL_ENGINE", default='django.db.backends.postgresql'),
+#         'NAME': env("SQL_DATABASE", default='diy_blog_prod'),
+#         "USER": env("SQL_USER", default="user"),
+#         "PASSWORD": env("SQL_PASSWORD", default="password"),
+#         "HOST": env("SQL_HOST", default="localhost"),
+#         "PORT": env("SQL_PORT", default="5432"),
+#     }
+# }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
