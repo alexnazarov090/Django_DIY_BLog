6393703f6da18b0553e54bac80d5071d6709c66f
Alex Nazarov <sladkii_bubaleg@yahoo.com>
2022-12-11 17:22:52 +0300

    Changed database setting in prod

--- diyblog/settings/production.py
+++ diyblog/settings/production.py
@@ -42,10 +42,10 @@ DATABASES = {
 }
 
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-DATABASES = {'default': dj_database_url.config(default=env("DB_CONNECTION_STRING", default='postgres://...'))}
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+# DATABASES = {'default': dj_database_url.config(default=env("DB_CONNECTION_STRING", default='postgres://...'))}
 
 # # Simplified static file serving.
 # # https://warehouse.python.org/project/whitenoise/
